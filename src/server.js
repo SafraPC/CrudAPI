edfc01cde8503d92c8cc0bdfc5045e0c43e7aedc
Leandro Safra <59981353+SafraPC@users.noreply.github.com>
2021-05-13 12:32:35 -0300

    can use in all frontend apps

--- src/server.js
+++ src/server.js
@@ -1,18 +1,18 @@
-const connection = require('./database');
-const express = require('express');
-const router = require('./routes');
-const path = require('path');
+const connection = require("./database");
+const express = require("express");
+const router = require("./routes");
+const path = require("path");
 const app = express();
-
-(async()=>{
-app.use(express.json());
-app.use(express.urlencoded({extended:false}));
-
-await connection();
-app.use(express.json());
-app.use(router);
-app.use('/uploads',express.static(path.resolve(__dirname,'..','uploads')));
-app.listen(8080,()=>{
-    console.log('Server is Running')
-});
-})()
+const cors = require("cors");
+(async () => {
+  app.use(express.json());
+  app.use(cors());
+  app.use(express.urlencoded({ extended: false }));
+  await connection();
+  app.use(express.json());
+  app.use(router);
+  app.use("/uploads", express.static(path.resolve(__dirname, "..", "uploads")));
+  app.listen(8080, () => {
+    console.log("Server is Running");
+  });
+})();
